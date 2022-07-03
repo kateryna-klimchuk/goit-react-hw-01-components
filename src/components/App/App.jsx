875d6f3547cea21e09322320f6eea778282e7e8f
kateryna klimchuk <ketyclima@gmail.com>
2022-07-03 10:15:04 +0300

    add profile files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,21 @@
 import css from './App.module.css';
 
+import Profile from 'components/Profile';
+import user from 'data/user.json';
+
+
 const App = () => {
-  return <div className={css.container}>React homework template</div>;
+  return (
+    <div className={css.container}>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
 };
 
 export default App;
